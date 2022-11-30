13d4467c3b54ebc72f2e978cc812ea963775a298
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2022-11-30 23:12:20 +0100

    Completed form validation

--- main.js
+++ main.js
@@ -6,12 +6,29 @@ const cardExpMonthDisplay = document.querySelector(".card-exp-month");
 const cardExpYearDisplay = document.querySelector(".card-exp-year");
 const cardCvcDisplay = document.querySelector(".card-cvc");
 
-// Credit card inputs
+// Form and inputs
+const form = document.querySelector("#form");
 const cardNameInput = document.querySelector("#cardholder-name");
 const cardNumberInput = document.querySelector("#card-number");
 const cardExpMonthInput = document.querySelector("#exp-date-month");
 const cardExpYearInput = document.querySelector("#exp-date-year");
 const cardCvcInput = document.querySelector("#card-cvc");
+
+// Alert messages
+const alertCardName = document.querySelector("#alert-card-name");
+const alertCardNumber = document.querySelector("#alert-card-number");
+const alertCardDate = document.querySelector("#alert-card-date");
+const alertCardCvc = document.querySelector("#alert-card-cvc");
+
+// Validations
+let isNameOk = false;
+let isNumberOk = false;
+let isMonthOk = false;
+let isYearOk = false;
+let isCvcOk = false;
+
+// Continue button
+const continueButton = document.querySelector("#continue");
 
 // Regex
 const numberRegex = /([0-9]){16}/;
@@ -50,22 +67,79 @@ function displayInfo(display, input) {
 
 // VALIDATION
 
-submitButton.addEventListener("click", function(e) {
+form.addEventListener("submit", (e) => {
     
-    e.preventDefault();
+    resetForm();
 
     const cardNumber = cardNumberInput.value.replace(/\s/g, "");
-    // const cardNumber = cardNumberInput.value.replace(/\s/g, "");
 
-    if (cardNumber.match(numberRegex)) {
-        console.log("number!!!");
+    // Validate card name
+    const cardName = cardNameInput.value;
+    if (!cardName) {
+        alert(alertCardName, cardNameInput);
     } else {
-        console.log("not number");
+        isNameOk = true;
     }
 
+    // Validate card number
+    if (!cardNumber.match(numberRegex)) {
+        alert(alertCardNumber, cardNumberInput);
+    } else {
+        isNumberOk = true;
+    }
 
+    // Validate Exp. month date
+    const cardExpMonth = parseInt(cardExpMonthInput.value);
+    const cardExpYear = parseInt(cardExpYearInput.value);
+    if (!cardExpMonth) {
+        alert(alertCardDate, cardExpMonthInput);
+    } else if (cardExpMonth === 0 || cardExpMonth > 12) {
+        alertCardDate.innerHTML = "Month must be between 1 and 12"
+        alert(alertCardDate, cardExpMonthInput);
+    } else {
+        isMonthOk = true;
+        if (!cardExpYear) {
+            alert(alertCardDate, cardExpYearInput); 
+        } else {
+            isYearOk = true;
+        }
+    }
+
+    // Validate Cvc
+    const cardCvc = parseInt(cardCvcInput.value);
+    if (!cardCvc) {
+        alertCardCvc.innerHTML = "Must be a number"
+        alert(alertCardCvc, cardCvcInput);
+    } else {
+        isCvcOk = true;
+    }
+
+    // Check errors or submit
+    if (isNameOk && isNumberOk && isMonthOk && isYearOk && isCvcOk) {
+        e.preventDefault();
+        form.classList.add("hide-element");
+        document.querySelector(".complete-state").classList.remove("hide-element");
+    } else {
+        e.preventDefault();
+        console.log(isNameOk, isNumberOk, isMonthOk, isYearOk, isCvcOk);
+    }
+    
 });
 
-/*
-const cardNumber = parseInt(cardNumberInput.value.replace(/\s/g, ""));
-*/
+function alert(alertMessage, elementInput) {
+    alertMessage.classList.add("alert-message-show");
+    elementInput.classList.add("alert-input");
+}
+
+function resetForm() {
+    document.querySelectorAll(".alert-message-show").forEach(element => {
+        element.classList.remove("alert-message-show");
+    }); 
+    document.querySelectorAll(".alert-input").forEach(element => {
+        element.classList.remove("alert-input");
+    }); 
+}
+
+continueButton.addEventListener("click", () => {
+    location.reload();
+});
